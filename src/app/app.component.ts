2afafb2c8fd502b691d550808db8a28c262d379d
Kevin Galarza Vera <44540130+kgalarza95@users.noreply.github.com>
2023-12-02 01:18:33 -0500

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SpotifyService } from './services/spotify.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'proy_spotify';
+
+  constructor(private spotify: SpotifyService) {
+    this.spotify.getNewReleases();
+  }
 }
